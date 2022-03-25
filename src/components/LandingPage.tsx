b6c4b1af8189ef240e5783129d0e615d7d2aa94d
simon <simonaertsbecode@gmail.com>
2022-03-25 18:44:36 +0100

    pagination need refactor

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import Curriculum from '../pages/Curriculum';
 import Overview from '../pages/Overview';
 import Pagination from './Pagination';
@@ -6,119 +6,70 @@ import Projects from '../pages/Projects';
 
 import { motion } from 'framer-motion';
 
+const stateForAnimation = {
+   left: {
+      x: '-50vw',
+      scale: 0.8,
+      zIndex: 0,
+      transition: {
+         duration: 0.5,
+      },
+   },
+   main: {
+      x: '-0vw',
+      scale: 1,
+      zIndex: 2,
+      transition: {
+         duration: 0.5,
+      },
+   },
+   right: {
+      x: '50vw',
+      scale: 0.8,
+      zIndex: 0,
+      transition: {
+         duration: 0.5,
+      },
+   },
+};
+
 const LandingPage = () => {
    const [actviveBtn, setActiveBtn] = useState(2);
-   const [stateForAnimation, setStateForAnimation] = useState({
-      left: {
-         x: '-50vw',
-         scale: 0.8,
-         zIndex: 0,
-      },
-      main: {
-         x: '-0vw',
-         scale: 1,
-         zIndex: 2,
-      },
-      right: {
-         x: '50vw',
-         scale: 0.8,
-         zIndex: 0,
-      },
-      transition: {
-         delay: 0.5,
-      },
-   });
 
-   const setAnimation = useCallback(
-      (value: number) => {
-         const { left, main, right } = stateForAnimation;
-         if (value === 1) {
-            setStateForAnimation((prevState) => ({
-               ...prevState,
-               [left.x]: '0vw',
-               [left.scale]: 1,
-               [left.zIndex]: 2,
-               [main.x]: '50vw',
-               [main.scale]: 0.8,
-               [main.zIndex]: 1,
-               [right.x]: '100vw',
-               [right.scale]: 0.6,
-               [right.zIndex]: 0,
-            }));
-         } else if (value === 2) {
-            setStateForAnimation((prevState) => ({
-               ...prevState,
-               [left.x]: '-50vw',
-               [left.scale]: 0.8,
-               [left.zIndex]: 0,
-               [main.x]: '0vw',
-               [main.scale]: 1,
-               [main.zIndex]: 2,
-               [right.x]: '50vw',
-               [right.scale]: 0.8,
-               [right.zIndex]: 0,
-            }));
-         } else if (value === 3) {
-            setStateForAnimation((prevState) => ({
-               ...prevState,
-               [left.x]: '-100vw',
-               [left.scale]: 0.6,
-               [left.zIndex]: 0,
-               [main.x]: '-50vw',
-               [main.scale]: 0.8,
-               [main.zIndex]: 1,
-               [right.x]: '0vw',
-               [right.scale]: 1,
-               [right.zIndex]: 2,
-            }));
-         }
-      },
-      [actviveBtn, stateForAnimation]
-   );
-
-   // useEffect(() => {
-   //          const { left, main, right } = stateForAnimation;
-   //          if (actviveBtn === 1) {
-   //             setStateForAnimation((prevState) => ({
-   //                ...prevState,
-   //                [left.x]: '0vw',
-   //                [left.scale]: 1,
-   //                [left.zIndex]: 2,
-   //                [main.x]: '50vw',
-   //                [main.scale]: 0.8,
-   //                [main.zIndex]: 1,
-   //                [right.x]: '100vw',
-   //                [right.scale]: 0.6,
-   //                [right.zIndex]: 0,
-   //             }));
-   //          } else if (actviveBtn === 2) {
-   //             setStateForAnimation((prevState) => ({
-   //                ...prevState,
-   //                [left.x]: '-50vw',
-   //                [left.scale]: 0.8,
-   //                [left.zIndex]: 0,
-   //                [main.x]: '0vw',
-   //                [main.scale]: 1,
-   //                [main.zIndex]: 2,
-   //                [right.x]: '50vw',
-   //                [right.scale]: 0.8,
-   //                [right.zIndex]: 0,
-   //             }));
-   //          } else if (actviveBtn === 3) {
-   //             setStateForAnimation((prevState) => ({
-   //                ...prevState,
-   //                [left.x]: '-100vw',
-   //                [left.scale]: 0.6,
-   //                [left.zIndex]: 0,
-   //                [main.x]: '-50vw',
-   //                [main.scale]: 0.8,
-   //                [main.zIndex]: 1,
-   //                [right.x]: '0vw',
-   //                [right.scale]: 1,
-   //                [right.zIndex]: 2,
-   //             }));
-   //          }
-   // }, [actviveBtn, stateForAnimation]);
+   useLayoutEffect(() => {
+      const { left, main, right } = stateForAnimation;
+      if (actviveBtn === 1) {
+         left.x = '0vw';
+         left.scale = 1;
+         left.zIndex = 2;
+         main.x = '50vw';
+         main.scale = 0.8;
+         main.zIndex = 1;
+         right.x = '100vw';
+         right.scale = 0.6;
+         right.zIndex = 0;
+      } else if (actviveBtn === 2) {
+         left.x = '-50vw';
+         left.scale = 0.8;
+         left.zIndex = 0;
+         main.x = '0vw';
+         main.scale = 1;
+         main.zIndex = 2;
+         right.x = '50vw';
+         right.scale = 0.8;
+         right.zIndex = 0;
+      } else if (actviveBtn === 3) {
+         left.x = '-100vw';
+         left.scale = 0.6;
+         left.zIndex = 0;
+         main.x = '-50vw';
+         main.scale = 0.8;
+         main.zIndex = 1;
+         right.x = '0vw';
+         right.scale = 1;
+         right.zIndex = 2;
+      }
+   }, [actviveBtn]);
 
    console.log(stateForAnimation);
 
