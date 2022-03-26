6a9edb31211a714c115ecbcb460dfa4f47aace4c
simon <simonaertsbecode@gmail.com>
2022-03-26 14:06:53 +0100

    progresse on pagination

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -71,88 +71,54 @@ const stateForAnimation = [
    },
 ];
 
+interface itemAnimation {
+   x: string;
+   scale: number;
+   zIndex: number;
+   transition: {
+      duration: number;
+   };
+}
+
 const LandingPage = () => {
    const [actviveBtn, setActiveBtn] = useState(2);
 
-   // useLayoutEffect(() => {
-   //    const { left, main, right } = stateForAnimation;
-   //    if (actviveBtn === 1) {
-   //       left.x = '0vw';
-   //       left.scale = 1;
-   //       left.zIndex = 2;
-   //       main.x = '50vw';
-   //       main.scale = 0.8;
-   //       main.zIndex = 1;
-   //       right.x = '100vw';
-   //       right.scale = 0.6;
-   //       right.zIndex = 0;
-   //    } else if (actviveBtn === 2) {
-   //       left.x = '-50vw';
-   //       left.scale = 0.8;
-   //       left.zIndex = 0;
-   //       main.x = '0vw';
-   //       main.scale = 1;
-   //       main.zIndex = 2;
-   //       right.x = '50vw';
-   //       right.scale = 0.8;
-   //       right.zIndex = 0;
-   //    } else if (actviveBtn === 3) {
-   //       left.x = '-100vw';
-   //       left.scale = 0.6;
-   //       left.zIndex = 0;
-   //       main.x = '-50vw';
-   //       main.scale = 0.8;
-   //       main.zIndex = 1;
-   //       right.x = '0vw';
-   //       right.scale = 1;
-   //       right.zIndex = 2;
-   //    }
-   // }, [actviveBtn]);
+   const jsxComponents = [<Curriculum />, <Overview />, <Projects />];
+
+   const animationLogic = (values: itemAnimation, index: number) => {
+      const isPositive = index < actviveBtn ? false : true;
+      const diff = Math.abs(actviveBtn - index);
+
+      let scaleValue = (0.8 * diff) % (diff - 1);
+
+      console.log(scaleValue);
+
+      // if (scaleValue == isNaN) {
+      //    scaleValue = 0.8 * diff;
+      // }
+
+      values.x = !isPositive ? `-${diff * 50}vw` : `${diff * 50}vw`;
+      values.scale = scaleValue;
+      values.zIndex = diff;
+   };
 
    return (
       <>
          <section className='container'>
-            {/* <motion.section animate={stateForAnimation[0].left} className='left-page'>
-               <Curriculum />
-            </motion.section>
-            <motion.section animate={stateForAnimation.main} className='main-page'>
-               <Overview />
-            </motion.section>
-            <motion.section animate={stateForAnimation.right} className='right-page'>
-               <Projects />
-            </motion.section> */}
-
             {stateForAnimation.map((item, index) => {
                const { values } = item;
                if (actviveBtn === index) {
                   values.x = '0vw';
                   values.scale = 1;
                   values.zIndex = 2;
-               } else if (index < actviveBtn) {
-                  const diff = actviveBtn - index;
-                  let scaleValue: number;
-                  if ((0.8 * diff) % (diff - 1) !== NaN) {
-                     scaleValue = 0.8 * diff;
-                  } else {
-                     scaleValue = (0.8 * diff) % (diff - 1);
-                  }
-                  values.x = `-${diff * 50}vw`;
-                  values.scale = scaleValue;
-                  values.zIndex = diff;
-               } else if (index < actviveBtn) {
-                  //**implement logic for index superior to activBtn */
-                  const diff = actviveBtn + index;
-                  let scaleValue: number;
-                  if ((0.8 * diff) % (diff - 1) !== NaN) {
-                     scaleValue = 0.8 * diff;
-                  } else {
-                     scaleValue = (0.8 * diff) % (diff - 1);
-                  }
-                  values.x = `-${diff * 50}vw`;
-                  values.scale = scaleValue;
-                  values.zIndex = diff;
+               } else {
+                  animationLogic(values, index);
                }
-               return <motion.section key={item.name} animate={item.values} className={`${item.name}-page`}></motion.section>;
+               return (
+                  <motion.section key={item.name} animate={item.values} className={`${item.name}-page`}>
+                     {jsxComponents[index]}
+                  </motion.section>
+               );
             })}
             <Pagination setActiveBtn={setActiveBtn} activeBtn={actviveBtn} />
          </section>
