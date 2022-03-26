d0ce3e413f74c7305c66a09ecec035b08216cf00
simon <simonaertsbecode@gmail.com>
2022-03-26 10:54:08 +0100

    pagination in progress

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -74,44 +74,40 @@ const stateForAnimation = [
 const LandingPage = () => {
    const [actviveBtn, setActiveBtn] = useState(2);
 
-   const setStateForAnimation = () => {
-      stateForAnimation.map((item, index) => {});
-   };
-
-   useLayoutEffect(() => {
-      const { left, main, right } = stateForAnimation;
-      if (actviveBtn === 1) {
-         left.x = '0vw';
-         left.scale = 1;
-         left.zIndex = 2;
-         main.x = '50vw';
-         main.scale = 0.8;
-         main.zIndex = 1;
-         right.x = '100vw';
-         right.scale = 0.6;
-         right.zIndex = 0;
-      } else if (actviveBtn === 2) {
-         left.x = '-50vw';
-         left.scale = 0.8;
-         left.zIndex = 0;
-         main.x = '0vw';
-         main.scale = 1;
-         main.zIndex = 2;
-         right.x = '50vw';
-         right.scale = 0.8;
-         right.zIndex = 0;
-      } else if (actviveBtn === 3) {
-         left.x = '-100vw';
-         left.scale = 0.6;
-         left.zIndex = 0;
-         main.x = '-50vw';
-         main.scale = 0.8;
-         main.zIndex = 1;
-         right.x = '0vw';
-         right.scale = 1;
-         right.zIndex = 2;
-      }
-   }, [actviveBtn]);
+   // useLayoutEffect(() => {
+   //    const { left, main, right } = stateForAnimation;
+   //    if (actviveBtn === 1) {
+   //       left.x = '0vw';
+   //       left.scale = 1;
+   //       left.zIndex = 2;
+   //       main.x = '50vw';
+   //       main.scale = 0.8;
+   //       main.zIndex = 1;
+   //       right.x = '100vw';
+   //       right.scale = 0.6;
+   //       right.zIndex = 0;
+   //    } else if (actviveBtn === 2) {
+   //       left.x = '-50vw';
+   //       left.scale = 0.8;
+   //       left.zIndex = 0;
+   //       main.x = '0vw';
+   //       main.scale = 1;
+   //       main.zIndex = 2;
+   //       right.x = '50vw';
+   //       right.scale = 0.8;
+   //       right.zIndex = 0;
+   //    } else if (actviveBtn === 3) {
+   //       left.x = '-100vw';
+   //       left.scale = 0.6;
+   //       left.zIndex = 0;
+   //       main.x = '-50vw';
+   //       main.scale = 0.8;
+   //       main.zIndex = 1;
+   //       right.x = '0vw';
+   //       right.scale = 1;
+   //       right.zIndex = 2;
+   //    }
+   // }, [actviveBtn]);
 
    return (
       <>
@@ -132,8 +128,31 @@ const LandingPage = () => {
                   values.x = '0vw';
                   values.scale = 1;
                   values.zIndex = 2;
+               } else if (index < actviveBtn) {
+                  const diff = actviveBtn - index;
+                  let scaleValue: number;
+                  if ((0.8 * diff) % (diff - 1) !== NaN) {
+                     scaleValue = 0.8 * diff;
+                  } else {
+                     scaleValue = (0.8 * diff) % (diff - 1);
+                  }
+                  values.x = `-${diff * 50}vw`;
+                  values.scale = scaleValue;
+                  values.zIndex = diff;
+               } else if (index < actviveBtn) {
+                  //**implement logic for index superior to activBtn */
+                  const diff = actviveBtn + index;
+                  let scaleValue: number;
+                  if ((0.8 * diff) % (diff - 1) !== NaN) {
+                     scaleValue = 0.8 * diff;
+                  } else {
+                     scaleValue = (0.8 * diff) % (diff - 1);
+                  }
+                  values.x = `-${diff * 50}vw`;
+                  values.scale = scaleValue;
+                  values.zIndex = diff;
                }
-               return <motion.section animate={item.values} className={`${item.name}-page`}></motion.section>;
+               return <motion.section key={item.name} animate={item.values} className={`${item.name}-page`}></motion.section>;
             })}
             <Pagination setActiveBtn={setActiveBtn} activeBtn={actviveBtn} />
          </section>
