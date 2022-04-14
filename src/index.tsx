35994f41f3d728cee1a2dcf0aa56b650db6b8050
simon <simonaertsbecode@gmail.com>
2022-04-14 09:26:21 +0200

    tried with webpack

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './scss/index.scss';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
